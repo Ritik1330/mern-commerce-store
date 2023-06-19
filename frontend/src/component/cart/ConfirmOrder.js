4f6c1cf0cce1ac03bb92a5585ca0fb9cc69d19c0
Ritik <Ritikcollege@gmail.com>
2023-06-19 12:21:29 +0530

    forget pass & buy

--- frontend/src/component/cart/ConfirmOrder.js
+++ frontend/src/component/cart/ConfirmOrder.js
@@ -19,7 +19,6 @@ const ConfirmOrder = () => {
   );
 
   const shippingCharges = subtotal > 1000 ? 0 : 200;
-
   const tax = subtotal * 0.18;
 
   const totalPrice = subtotal + tax + shippingCharges;
