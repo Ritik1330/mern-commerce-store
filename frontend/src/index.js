99202ed8cc18eadceb1552902513a9b8f734004d
Ritik <Ritikcollege@gmail.com>
2023-07-03 21:45:02 +0530

    for deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from "react-dom/client";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import App from "./App";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <Elements stripe={stripePromise}>
-      <App />
-    </Elements>
+    <App />
   </Provider>
 );
